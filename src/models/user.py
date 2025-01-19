3bd229471f0f26509b3abfe982e01b6b3c1da4d9
Ksenia Panova <rybinak95@gmail.com>
2025-01-20 00:31:37 +0300

    Alter the phone field in the user model

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,6 @@
 import enum
 from datetime import date, datetime
-from sqlalchemy import Integer, DateTime, Date, String, Enum
+from sqlalchemy import Integer, DateTime, Date, String, CHAR, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 from src.database import Base
 
@@ -20,7 +20,7 @@ class User(Base):
     first_name: Mapped[str] = mapped_column(String(50), nullable=False)
     patronymic: Mapped[str | None] = mapped_column(String(40), nullable=True, default=None)
     birthday: Mapped[date] = mapped_column(Date, nullable=False)
-    phone: Mapped[str] = mapped_column(String(19), nullable=False, unique=True)
+    phone: Mapped[str] = mapped_column(CHAR(18), nullable=False, unique=True)
     email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     role: Mapped[UserRoleEnum] = mapped_column(Enum(UserRoleEnum), nullable=False)
     login: Mapped[str] = mapped_column(String(16), nullable=False, unique=True)
