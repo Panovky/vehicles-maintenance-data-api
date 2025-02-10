cbe4c40c9e0d04602e58c283af0c7c87edc1d229
Ksenia Panova <rybinak95@gmail.com>
2025-02-10 23:01:53 +0300

    Refactor users routes

--- src/exceptions.py
+++ src/exceptions.py
@@ -23,3 +23,35 @@ class RegistrationNumberIsNotUniqueException(HTTPException):
             status_code=status.HTTP_409_CONFLICT,
             detail='Vehicle with the same registration number already exists.'
         )
+
+
+class UserNotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User not found.'
+        )
+
+
+class UserPhoneIsNotUniqueException(HTTPException):
+    def __init__(self, role):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f'User with the same phone number already exists among users with role «{role}».'
+        )
+
+
+class UserEmailIsNotUniqueException(HTTPException):
+    def __init__(self, role):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f'User with the same email address already exists among users with role «{role}».'
+        )
+
+
+class UserLoginIsNotUniqueException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='User with the same login already exists.'
+        )
