6510aed7aa713f747154bf4de03c2e7456583fbe
Ksenia Panova <rybinak95@gmail.com>
2025-04-08 14:56:01 +0300

    Rename routes to api

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.routes import users, services, makes, models, ranges, generations, configurations, scrape, vehicles
+from src.api import users, services, makes, models, ranges, generations, configurations, scrape, vehicles
 
 app = FastAPI()
 
