97777b9754d4ed4cf5b9f8dc1b6488e0e39c9a41
Ksenia Panova <rybinak95@gmail.com>
2025-04-17 12:05:31 +0300

    Add auth schemas

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status
 from src.dependencies import AuthServiceDep
-from src.users.schemas import UserCreate, UserRead
+from src.users.schemas import UserRead
+from .schemas import UserRegister, UserLogin
 
 router = APIRouter(
     prefix='/auth',
@@ -14,14 +15,15 @@ router = APIRouter(
     responses={201: {'description': 'User successfully registered'}, 409: {'description': 'User data is not unique'}},
     summary='Register a user'
 )
-async def register(data: UserCreate, auth_service: AuthServiceDep) -> UserRead:
+async def register(data: UserRegister, auth_service: AuthServiceDep) -> UserRead:
     user = await auth_service.register(data)
     return user
 
 
 @router.post('/login')
-async def login():
-    pass
+async def login(data: UserLogin, auth_service: AuthServiceDep):
+    res = auth_service.login()
+    return res
 
 
 @router.post('/refresh')
