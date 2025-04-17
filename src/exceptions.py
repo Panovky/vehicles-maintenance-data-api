00ac942e94c3ddb75b2b53c844bcc9e2c28a46cd
Ksenia Panova <rybinak95@gmail.com>
2025-04-17 12:30:55 +0300

    Add http exception for invalid user credentials

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,6 +2,14 @@ from fastapi import HTTPException
 from starlette import status
 
 
+class InvalidUserCredentialsException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Неверный e-mail или пароль.'
+        )
+
+
 class MakeNotFoundException(HTTPException):
     def __init__(self):
         super().__init__(
