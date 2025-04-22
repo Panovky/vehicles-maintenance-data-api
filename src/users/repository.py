1c986855cde17033d00f4ead6f7c223c7be79258
Ksenia Panova <rybinak95@gmail.com>
2025-04-22 16:46:31 +0300

    Add joined roles in get user by email

--- src/users/repository.py
+++ src/users/repository.py
@@ -1,5 +1,6 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 from src.utils.sqlalchemy_repository import SQLAlchemyRepository
 from .model import User, UserRole, RoleEnum
 
@@ -9,7 +10,7 @@ class UsersRepository(SQLAlchemyRepository):
         super().__init__(async_session, User)
 
     async def get_by_email(self, email: str) -> User | None:
-        stmt = select(User).where(User.email == email)
+        stmt = select(User).where(User.email == email).options(joinedload(User.roles))
         user = await self.async_session.execute(stmt)
         return user.scalar()
 
