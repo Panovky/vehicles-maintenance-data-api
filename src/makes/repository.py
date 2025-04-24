8d1d84819af57f9700e8a15b9ca65570c0681a12
Ksenia Panova <rybinak95@gmail.com>
2025-04-25 00:21:29 +0300

    Add method get by name in makes repository

--- src/makes/repository.py
+++ src/makes/repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.utils.sqlalchemy_repository import SQLAlchemyRepository
 from .model import Make
@@ -6,3 +7,8 @@ from .model import Make
 class MakesRepository(SQLAlchemyRepository):
     def __init__(self, async_session: AsyncSession):
         super().__init__(async_session, Make)
+
+    async def get_by_name(self, name: str) -> Make | None:
+        stmt = select(Make).where(Make.name == name)
+        make = await self.async_session.execute(stmt)
+        return make.scalar()
