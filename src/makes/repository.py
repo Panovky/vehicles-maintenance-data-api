f2bcab43706c95e1ce43731805d234aada8e4cac
Ksenia Panova <rybinak95@gmail.com>
2025-04-25 15:25:25 +0300

    Delete unused method from makes repository

--- src/makes/repository.py
+++ src/makes/repository.py
@@ -1,4 +1,3 @@
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.utils.sqlalchemy_repository import SQLAlchemyRepository
 from .model import Make
@@ -7,8 +6,3 @@ from .model import Make
 class MakesRepository(SQLAlchemyRepository):
     def __init__(self, async_session: AsyncSession):
         super().__init__(async_session, Make)
-
-    async def get_by_name(self, name: str) -> Make | None:
-        stmt = select(Make).where(Make.name == name)
-        make = await self.async_session.execute(stmt)
-        return make.scalar()
