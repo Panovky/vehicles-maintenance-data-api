cd8e682c693280da917cdcf1b221e91324b22012
Ksenia Panova <rybinak95@gmail.com>
2025-04-25 15:24:47 +0300

    Refactor schemas

--- src/makes/schemas.py
+++ src/makes/schemas.py
@@ -3,11 +3,6 @@ from pydantic import BaseModel, Field, ConfigDict
 from typing import Annotated
 
 
-class MakeCreate(BaseModel):
-    """The model representing the vehicle make data needed to create record in the database."""
-    name: Annotated[str, Field(max_length=40, example='Renault')]
-
-
 class MakeRead(BaseModel):
     """The model representing the vehicle make data to be returned to the client."""
     model_config = ConfigDict(from_attributes=True)
--- src/scrapers/schemas.py
+++ src/scrapers/schemas.py
@@ -1,53 +1,5 @@
 from pydantic import BaseModel, Field
 from typing import Annotated
-from src.models.model import ModelTypeEnum
-
-
-class ModelScrape(BaseModel):
-    """
-    The model representing the data of vehicle model scraped from drom.ru.
-
-    The model used for further data scraping.
-    """
-    name: str
-    type: ModelTypeEnum
-    ranges_and_generations_drom_url: str
-
-
-class GenerationScrape(BaseModel):
-    """
-    The model representing the data of vehicle generation scraped from drom.ru.
-
-    The model used for further data scraping.
-    """
-    photo_url: str
-    full_name: str
-    short_name: str
-    vehicle_body: str
-    configurations_drom_url: str
-
-
-class RangeScrape(BaseModel):
-    """
-    The model representing the data of vehicle models range scraped from drom.ru.
-
-    The model used for further data scraping.
-    """
-    name: str
-    generations: list[GenerationScrape]
-
-
-class ConfigurationScrape(BaseModel):
-    """
-    The model representing the data of vehicle configurations scraped from drom.ru.
-
-    The model used for further data scraping.
-    """
-    engine_capacity: float | None
-    engine_power: int | None
-    engine_type: str | None
-    transmission: str | None
-    drive: str | None
 
 
 class UnhandledDromResponseErrorRead(BaseModel):
@@ -59,5 +11,5 @@ class UnhandledDromResponseErrorRead(BaseModel):
 
 
 class UnhandledDromResponseErrorsRead(BaseModel):
-    """The model describes the list of errors that occurred when receiving a response from the drom.ru."""
+    """The model describes the response to a request for data scraping from drom.ru."""
     unhandled_errors: list[UnhandledDromResponseErrorRead]
