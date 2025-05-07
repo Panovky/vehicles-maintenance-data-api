9af2c438dd765ff1ecc8aa07cb6256e01fd908f4
Ksenia Panova <rybinak95@gmail.com>
2025-05-07 23:59:07 +0300

    Add method to create vehicle in vehicles repository

--- src/vehicles/repository.py
+++ src/vehicles/repository.py
@@ -6,3 +6,19 @@ from .model import Vehicle
 class VehiclesRepository(SQLAlchemyRepository):
     def __init__(self, async_session: AsyncSession):
         super().__init__(async_session, Vehicle)
+
+    async def create(self, data: dict) -> Vehicle:
+        vehicle = self.model(**data)
+        self.async_session.add(vehicle)
+        await self.async_session.commit()
+        await self.async_session.refresh(
+            vehicle,
+            [
+                'make',
+                'model',
+                'range',
+                'generation',
+                'configuration'
+            ]
+        )
+        return vehicle
