d1f5b6455957a1a362c29d4c59a9c31a804f0af7
Ksenia Panova <rybinak95@gmail.com>
2025-05-08 17:15:51 +0300

    Refactor vehicles pydantic schemas

--- src/vehicles/schemas.py
+++ src/vehicles/schemas.py
@@ -17,6 +17,7 @@ class VehicleRead(BaseModel):
     created_at: datetime
     updated_at: datetime
     user_id: Annotated[int, Field(example=1)]
+    photo_path: Annotated[str, Field(example='/static/vehicles/photos/JHMCM56557C404453.jpg')]
     make: MakeRead
     model: ModelRead
     range: RangeRead
@@ -26,20 +27,4 @@ class VehicleRead(BaseModel):
     manufacture_year: Annotated[int, Field(example=2005)]
     mileage: Annotated[int, Field(example=175000)]
     vin: Annotated[str, Field(example='JHMCM56557C404453')]
-    registration_plate: Annotated[str, Field(example='Е734МТ76')]
-
-
-class VehicleCreate(BaseModel):
-    """The model representing the vehicle data needed to create record in the database."""
-    make_id: Annotated[int, Field(gt=0, example=1)]
-    model_id: Annotated[int, Field(gt=0, example=1)]
-    range_id: Annotated[int, Field(gt=0, example=1)]
-    generation_id: Annotated[int, Field(gt=0, example=1)]
-    configuration_id: Annotated[int, Field(gt=0, example=1)]
-    color: VehicleColorEnum
-    manufacture_year: Annotated[int, Field(gte=1900, example=2005)]
-    mileage: Annotated[int, Field(gte=0, example=175000)]
-    vin: Annotated[str, Field(pattern='^[A-HJ-NPR-Z0-9]{17}$', example='JHMCM56557C404453')]
-    registration_plate: Annotated[str, Field(
-        pattern='^[АВЕКМНОРСТУХ]{1}[0-9]{3}[АВЕКМНОРСТУХ]{2}[0-9]{2,3}$', example='Е734МТ76'
-    )]
+    registration_plate: Annotated[str, Field(example='E734MT76')]
