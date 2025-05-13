55cfdc5b2b6adcbcb61701c446e33b152cb83e34
Ksenia Panova <rybinak95@gmail.com>
2025-05-13 14:24:02 +0300

    Add service_workers table

---
+++ src/service_workers/model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String, Integer, Float, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from src.utils.base_model import Base
+
+
+class ServiceWorker(Base):
+    __tablename__ = 'service_workers'
+
+    service_id: Mapped[int] = mapped_column(Integer, ForeignKey('services.id'))
+    worker_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
+    position: Mapped[str] = mapped_column(String(100), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False, default=0)
