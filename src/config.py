65ef1ae978f0e2d1c71cfb4e7b7b5c9db1c58ad6
Ksenia Panova <rybinak95@gmail.com>
2025-05-14 00:38:53 +0300

    Add method to get invite worker token

--- src/config.py
+++ src/config.py
@@ -7,17 +7,18 @@ STATIC_DIR = BASE_DIR / 'static'
 VEHICLES_PHOTOS_DIR = STATIC_DIR / 'vehicles' / 'photos'
 
 
-class JWTAuth(BaseModel):
+class JWTSettings(BaseModel):
     private_key_path: Path = BASE_DIR / 'certs' / 'private.pem'
     public_key_path: Path = BASE_DIR / 'certs' / 'public.pem'
     algorithm: str = 'RS256'
     verify_email_token_expire_hours: int = 24
     access_token_expire_minutes: int = 15
     refresh_token_expire_days: int = 30
+    invite_worker_token_expire_hours: int = 24
 
 
 class Settings(BaseSettings):
-    jwt_auth: JWTAuth = JWTAuth()
+    jwt: JWTSettings = JWTSettings()
 
 
 settings = Settings()
--- src/core/jwt_service.py
+++ src/core/jwt_service.py
@@ -8,8 +8,8 @@ class JWTService:
     def encode_jwt(
             payload: dict,
             token_expire_minutes: int,
-            private_key: str = settings.jwt_auth.private_key_path.read_text(),
-            algorithm: str = settings.jwt_auth.algorithm
+            private_key: str = settings.jwt.private_key_path.read_text(),
+            algorithm: str = settings.jwt.algorithm
     ) -> str:
         payload_to_encode = payload.copy()
         now = datetime.utcnow()
@@ -20,25 +20,31 @@ class JWTService:
     @staticmethod
     def decode_jwt(
             token: str,
-            public_key: str = settings.jwt_auth.public_key_path.read_text(),
-            algorithm: str = settings.jwt_auth.algorithm
+            public_key: str = settings.jwt.public_key_path.read_text(),
+            algorithm: str = settings.jwt.algorithm
     ) -> str:
         return jwt.decode(token, public_key, algorithms=[algorithm])
 
     def get_access_token(self, _id: int, email: str) -> str:
         return self.encode_jwt(
             payload={'sub': str(_id), 'email': email, 'type': 'access'},
-            token_expire_minutes=settings.jwt_auth.access_token_expire_minutes
+            token_expire_minutes=settings.jwt.access_token_expire_minutes
         )
 
     def get_refresh_token(self, _id: int, email: str) -> str:
         return self.encode_jwt(
             payload={'sub': str(_id), 'email': email, 'type': 'refresh'},
-            token_expire_minutes=settings.jwt_auth.refresh_token_expire_days * 24 * 60
+            token_expire_minutes=settings.jwt.refresh_token_expire_days * 24 * 60
         )
 
     def get_verify_email_token(self, email: str, role: str) -> str:
         return self.encode_jwt(
             payload={'sub': email, 'role': role, 'type': 'verify_email'},
-            token_expire_minutes=settings.jwt_auth.verify_email_token_expire_hours * 60
+            token_expire_minutes=settings.jwt.verify_email_token_expire_hours * 60
+        )
+
+    def get_invite_worker_token(self, email: str, position: str) -> str:
+        return self.encode_jwt(
+            payload={'sub': email, 'position': position, 'type': 'invite_worker'},
+            token_expire_minutes=settings.jwt.invite_worker_token_expire_hours * 60
         )
