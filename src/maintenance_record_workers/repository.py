00fc2b4d13be2537094dbd996aa5696999793c9f
Ksenia Panova <rybinak95@gmail.com>
2025-05-23 13:00:33 +0300

    Add schema for maintenance record workers

--- src/maintenance_record_workers/repository.py
+++ src/maintenance_record_workers/repository.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.core.sqlalchemy_repository import SQLAlchemyRepository
-from .model import MaintenanceRecordWorker
+from .model import MaintenanceRecordServiceWorker
 
 
 class MaintenanceRecordWorkersRepository(SQLAlchemyRepository):
     def __init__(self, async_session: AsyncSession):
-        super().__init__(async_session, MaintenanceRecordWorker)
+        super().__init__(async_session, MaintenanceRecordServiceWorker)
