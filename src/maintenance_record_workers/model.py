220dd3e60d71aa1999e3643c3c7f6cb9092dfc51
Ksenia Panova <rybinak95@gmail.com>
2025-05-23 12:42:24 +0300

    Rename table maintenance record workers

--- src/maintenance_record_workers/model.py
+++ src/maintenance_record_workers/model.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.core.base_model import Base
 
 
-class MaintenanceRecordWorker(Base):
-    __tablename__ = 'maintenance_record_workers'
+class MaintenanceRecordServiceWorker(Base):
+    __tablename__ = 'maintenance_record_service_workers'
 
     maintenance_record_id: Mapped[int] = mapped_column(Integer, ForeignKey('maintenance_records.id'))
-    worker_id: Mapped[int] = mapped_column(Integer, ForeignKey('service_workers.id'))
+    service_worker_id: Mapped[int] = mapped_column(Integer, ForeignKey('service_workers.id'))
