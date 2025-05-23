8b386c209e5585b17ff892a973fd2e6f49bbb083
Ksenia Panova <rybinak95@gmail.com>
2025-05-23 13:50:19 +0300

    Rename worker id to user id

--- src/service_workers/model.py
+++ src/service_workers/model.py
@@ -7,7 +7,7 @@ class ServiceWorker(Base):
     __tablename__ = 'service_workers'
 
     service_id: Mapped[int] = mapped_column(Integer, ForeignKey('services.id'))
-    worker_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
     position: Mapped[str] = mapped_column(String(100), nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=False, default=0)
 
