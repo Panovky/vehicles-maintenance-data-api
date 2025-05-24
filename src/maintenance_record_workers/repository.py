78e0019c822b21877dc2a1603d26f8d912ea2a3b
Ksenia Panova <rybinak95@gmail.com>
2025-05-24 22:57:12 +0300

    Rename maintenance record service workers directory

--- src/maintenance_record_workers/repository.py
+++ src/maintenance_record_workers/repository.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.core.sqlalchemy_repository import SQLAlchemyRepository
-from .model import MaintenanceRecordServiceWorker
+from .model import MaintenanceRecordWorker
 
 
 class MaintenanceRecordWorkersRepository(SQLAlchemyRepository):
     def __init__(self, async_session: AsyncSession):
-        super().__init__(async_session, MaintenanceRecordServiceWorker)
+        super().__init__(async_session, MaintenanceRecordWorker)
