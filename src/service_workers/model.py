37c8d77d3587f32f81df70f7220edc8d3e6e4df5
Ksenia Panova <rybinak95@gmail.com>
2025-05-24 19:58:09 +0300

    Refactor service worker and service client models

--- src/service_workers/model.py
+++ src/service_workers/model.py
@@ -7,12 +7,12 @@ class ServiceWorker(Base):
     __tablename__ = 'service_workers'
 
     service_id: Mapped[int] = mapped_column(Integer, ForeignKey('services.id'))
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
+    worker_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
     position: Mapped[str] = mapped_column(String(100), nullable=False)
     ratings_sum: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
     ratings_count: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
     rating: Mapped[float] = mapped_column(Float, nullable=False, default=0)
 
-    user = relationship('User', back_populates='service_workers')
+    worker = relationship('User', back_populates='service_workers')
     maintenance_records = relationship('MaintenanceRecord', back_populates='responsible')
     maintenance_record_service_workers = relationship('MaintenanceRecordServiceWorker', back_populates='service_worker')
