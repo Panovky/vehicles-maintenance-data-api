dcb0b3b2e96d161a7ccf399e961b55ad2e7482fa
Ksenia Panova <rybinak95@gmail.com>
2025-06-07 23:41:32 +0300

    Add route to detach service worker

--- src/service_workers/routes.py
+++ src/service_workers/routes.py
@@ -10,6 +10,28 @@ router = APIRouter(
 
 
 @router.post(
+    '/services/{service_id}/workers/{worker_id}/rate',
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={
+        204: {'description': 'Service worker rating successfully completed'},
+        401: {'description': 'Access token are invalid'},
+        403: {'description': 'Access for current user denied'},
+        404: {'description': 'Service or worker not found'}
+    },
+    summary='Rate the service worker by current owner'
+)
+async def rate_service_worker(
+        current_owner: CurrentOwnerDep,
+        service_id: Annotated[int, Path(gt=0)],
+        worker_id: Annotated[int, Path(gt=0)],
+        rating: Annotated[int, Body(embed=True, gt=0, lt=6)],
+        service_workers_service: ServiceWorkersServiceDep
+) -> Response:
+    await service_workers_service.rate_service_worker(service_id, worker_id, rating)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.post(
     '/services/{service_id}/workers/invite',
     status_code=status.HTTP_204_NO_CONTENT,
     responses={
@@ -43,25 +65,24 @@ async def attach_worker(
     return await service_workers_service.attach_worker(service_id, token)
 
 
-@router.post(
-    '/services/{service_id}/workers/{worker_id}/rate',
+@router.delete(
+    '/services/{service_id}/workers/{worker_id}',
     status_code=status.HTTP_204_NO_CONTENT,
     responses={
-        204: {'description': 'Service worker rating successfully completed'},
+        204: {'description': 'The worker successfully detached from the service'},
         401: {'description': 'Access token are invalid'},
         403: {'description': 'Access for current user denied'},
         404: {'description': 'Service or worker not found'}
     },
-    summary='Rate the service worker by current owner'
+    summary='Detach the worker from the service'
 )
-async def rate_service_worker(
-        current_owner: CurrentOwnerDep,
+async def detach_worker(
+        current_manager: CurrentManagerDep,
         service_id: Annotated[int, Path(gt=0)],
         worker_id: Annotated[int, Path(gt=0)],
-        rating: Annotated[int, Body(embed=True, gt=0, lt=6)],
         service_workers_service: ServiceWorkersServiceDep
 ) -> Response:
-    await service_workers_service.rate_service_worker(service_id, worker_id, rating)
+    await service_workers_service.detach_worker(service_id, worker_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
